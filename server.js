46a44588d77e0e640ff498a63ebf50c63875f564
mariasalomevillarreal <msvillarreal2021@gmail.com>
2021-09-27 17:29:29 -0500

    update server.js

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const routes = require('./Develop/routes');
 // import sequelize connection
 
+const sequelize = require('./Develop/config/connection');
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -11,6 +13,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
+sequelize.sync({ force:false }).then(() => {
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
+});  
 });
